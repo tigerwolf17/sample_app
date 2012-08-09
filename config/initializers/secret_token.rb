f176f70738f05f9d1b7aa2edbfcbb973fedbab5a
Wolf <ilya.yeryomin@gmail.com>
2012-08-09 10:45:27 +0300

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EclipseProject::Application.config.secret_token = '364bdf3663c6eccfb5613c702a737764f373bde3bdf7d85a8e37611ec2a9922b5f0a5ba24d5208d462ad11a5e09c312b143c683b64df923688259f9e0ffdf5cc'
